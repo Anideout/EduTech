1534dd4fc657764d5aa3a161828f8fe9f2cee013
maruchandepollo <166879059+maruchandepollo@users.noreply.github.com>
2025-05-26 20:12:12 -0400

    Delete edutech/src/main/java/com/edutech/edutech/repository/NotificacionRepository.java

--- edutech/src/main/java/com/edutech/edutech/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.edutech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.edutech.model.Notificacion;
-
-public interface NotificacionRepository extends JpaRepository<Notificacion, Integer> {
-    Notificacion findById(int id);
-}
